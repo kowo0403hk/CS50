ac742d350b772b6b1ccbce0414a079515e16d9a0
Alex Cheung <alexc.sfu@gmail.com>
2022-09-02 13:27:10 -0700

    added a cs50 library to the hello.c file

--- Week 1/hello.c
+++ Week 1/hello.c
@@ -1,6 +1,13 @@
+#include <cs50.h>
 #include <stdio.h>
+
+// int main(void)
+// {
+//   printf("hello, world!\n");
+// }
 
 int main(void)
 {
-  printf("hello, world!\n");
+  string answer = get_string("What is your name? ");
+  printf("hello, %s\n", answer);
 }
